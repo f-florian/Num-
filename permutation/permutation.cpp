5df1023c0d1e33f7daad2998cb61af9c329440a4
Francesco Florian <francesco.florian94@gmail.com>
2018-03-10 13:08:55 +0100

    migrating to std::vector

--- permutation/permutation.cpp
+++ permutation/permutation.cpp
@@ -22,119 +22,124 @@
 using namespace std;
 
 Permutation::Permutation(size_t maximum)
-	:maximum{maximum}
+    :maximum{maximum}
 {
-	permutation=new size_t[maximum];
+    permutation.reserve(maximum);
+    for (int i = 0; i < maximum; ++i) {
+        permutation[i]=i;
+    }
 }
 Permutation::Permutation(size_t maximum, size_t e1, size_t e2)
-	:maximum{maximum}
+    :maximum{maximum}
 {
-	permutation=new size_t[maximum];
-	if(e1>maximum||e2>maximum)
-		throw(range_error("Attempt to permute elements out of range"));
-	permutation[e1]=e2;
-	permutation[e2]=e1;
+    permutation.reserve(maximum);
+    for (int i = 0; i < maximum; ++i) {
+        permutation[i]=i;
+    }
+    if(e1>maximum||e2>maximum)
+        throw(range_error("Attempt to permute elements out of range"));
+    permutation[e1]=e2;
+    permutation[e2]=e1;
 }
 Permutation::Permutation(size_t maximum, initializer_list<size_t> list)
-	:maximum{maximum}
+    :maximum(maximum)
+     :permutation(list)
 {
-	if(maximum<list.size())
-		throw(range_error("Triyng to use an initializer_list to init a storter array"));
-	permutation=new size_t[maximum];
-	copy(list.begin(),list.end(),permutation);
-	//todo: range check?
+    if(maximum<list.size())
+        throw(range_error("Triyng to use an initializer_list to init a storter array"));
+    //todo: range check?
 }
 Permutation::Permutation(initializer_list<size_t> list):Permutation(list.size(),list){}
 Permutation::Permutation(const Permutation & other)
-	:maximum{other.maximum}
+    :maximum{other.maximum}
 {
-	permutation=new size_t[maximum];
-	memcpy(permutation, other.permutation, maximum*sizeof(size_t));
+    permutation=new size_t[maximum];
+    memcpy(permutation, other.permutation, maximum*sizeof(size_t));
 }
 Permutation::Permutation(Permutation && other) noexcept
 {
-	maximum=other.maximum;
-	permutation=other.permutation;
-	other.permutation=nullptr;
+    maximum=other.maximum;
+    permutation=other.permutation;
+    other.permutation=nullptr;
 }
 Permutation::~Permutation() noexcept
 {
-	delete[] permutation;
+    delete[] permutation;
 }
 Permutation& Permutation::operator= (Permutation &&other) noexcept
 {
-	maximum=other.maximum;
-	swap(permutation,other.permutation);
+    maximum=other.maximum;
+    swap(permutation,other.permutation);
 }
 	
 Permutation Permutation::operator* (const Permutation &other) const
 {
-	Permutation result(max(maximum,other.maximum));
-	size_t minmax=min(maximum,other.maximum);
-	for(size_t i=0;i<minmax;i++)
-		result.permutation[i]=permutation[other.permutation[i]];
-	if(maximum>other.maximum){
-		memcpy(result.permutation,&permutation[minmax],maximum-minmax);
-	} else {
-		for(size_t i=minmax;i<other.maximum;i++)
-			if(other.permutation[i]<minmax)
-				result.permutation[i]=permutation[other.permutation[i]];
-			else
-				result.permutation[i]=other.permutation[i];
-	}
+    Permutation result(max(maximum,other.maximum));
+    size_t minmax=min(maximum,other.maximum);
+    for(size_t i=0;i<minmax;i++)
+        result.permutation[i]=permutation[other.permutation[i]];
+    if(maximum>other.maximum){
+        memcpy(result.permutation,&permutation[minmax],maximum-minmax);
+    } else {
+        for(size_t i=minmax;i<other.maximum;i++)
+            if(other.permutation[i]<minmax)
+                result.permutation[i]=permutation[other.permutation[i]];
+            else
+                result.permutation[i]=other.permutation[i];
+    }
 }
 void Permutation::operator*= (const Permutation &other)
 {
-	if(other.maximum>maximum){
-		size_t *tmp=static_cast<size_t*>(realloc(permutation,other.maximum*sizeof(size_t)));
-		if(tmp==NULL)
-			throw(bad_alloc());
-		permutation=tmp;
-	}
-	maximum=other.maximum;
-	bool ok[maximum];
-	memset(ok,0,sizeof(bool));
-	for(size_t i=0;i<maximum;i++){
-		if(ok[i])
-			continue;
-		size_t k=permutation[i];
-		size_t l=i;
-		size_t j;
-		for(;;){
-			j=other.permutation[l];
-			if(j==i)
-				break;
-			permutation[l]=permutation[j];
-			ok[l]=true;
-			l=j;
-		}
-		permutation[l]=k;
-	}
-  // todo: undefined sometimes
+    if(other.maximum>maximum){
+        size_t *tmp=static_cast<size_t*>(realloc(permutation,other.maximum*sizeof(size_t)));
+        if(tmp==NULL)
+            throw(bad_alloc());
+        permutation=tmp;
+    }
+    maximum=other.maximum;
+    bool ok[maximum];
+    memset(ok,0,sizeof(bool));
+    for(size_t i=0;i<maximum;i++){
+        if(ok[i])
+            continue;
+        size_t k=permutation[i];
+        size_t l=i;
+        size_t j;
+        for(;;){
+            j=other.permutation[l];
+            if(j==i)
+                break;
+            permutation[l]=permutation[j];
+            ok[l]=true;
+            l=j;
+        }
+        permutation[l]=k;
+    }
+    // todo: undefined sometimes
 }
 Permutation Permutation::inverse() const
 {
-	Permutation result(maximum);
-	for(size_t i=0;i<maximum;i++)
-		result.permutation[permutation[i]]=i;
+    Permutation result(maximum);
+    for(size_t i=0;i<maximum;i++)
+        result.permutation[permutation[i]]=i;
 }
 bool Permutation::operator==(const Permutation &other) const noexcept
 {
-	if(maximum!=other.maximum)
-		return false;
-	for(size_t i=0;i<maximum;i++)
-		if(permutation[i]!=other.permutation[i])
-			return false;
-	return true;
+    if(maximum!=other.maximum)
+        return false;
+    for(size_t i=0;i<maximum;i++)
+        if(permutation[i]!=other.permutation[i])
+            return false;
+    return true;
 }
 bool Permutation::operator!=(const Permutation &other) const noexcept
 {
-	if(maximum!=other.maximum)
-		return true;
-	for(size_t i=0;i<maximum;i++)
-		if(permutation[i]!=other.permutation[i])
-			return true;
-	return false;
+    if(maximum!=other.maximum)
+        return true;
+    for(size_t i=0;i<maximum;i++)
+        if(permutation[i]!=other.permutation[i])
+            return true;
+    return false;
 }
 bool Permutation::operator<=(const Permutation &other) const noexcept {return (*this)==other;}
 bool Permutation::operator>=(const Permutation &other) const noexcept {return (*this)==other;}
@@ -143,57 +148,57 @@ bool Permutation::operator>(const Permutation &other) const noexcept {return (*t
 
 string Permutation::print() const noexcept
 {
-	string out;
-	if(maximum>0){
-		out=to_string(permutation[0]);
-		for(size_t i=1;i<maximum;i++)
-			out+="\t"+to_string(permutation[i]);
-	}
-	return out;
+    string out;
+    if(maximum>0){
+        out=to_string(permutation[0]);
+        for(size_t i=1;i<maximum;i++)
+            out+="\t"+to_string(permutation[i]);
+    }
+    return out;
 }
 size_t Permutation::operator[](const size_t point) const
 {
-	if(point>maximum)
-		throw(range_error("Requested element out of range"));
-	return permutation[point];
+    if(point>maximum)
+        throw(range_error("Requested element out of range"));
+    return permutation[point];
 }
 void Permutation::swap(const size_t x1, const size_t x2)
 {
-	m=min(x1,x2);
-	M=x1-m+x2;                  //max(x1,x2)
-	if(M<maximum){
-		size_t tmp=permutation[x1];
-		permutation[x1]=permutation[x2];
-		permutation[x2]=tmp;
-		return;
-	}
-	size_t *tmp=static_cast<size_t*>(realloc(M));
-	if(tmp==NULL)
-		throw(bad_alloc());
-	permutation=tmp;
-	// TODO: fill, or change operator[]
-	if (m<maximum) {
-		permutation[M]=m;
-	} else {
-		permutation[M]=permutation[m];
-	}
-	permutation[m]=M;
-	maximum=M;
+    m=min(x1,x2);
+    M=x1-m+x2;                  //max(x1,x2)
+    if(M<maximum){
+        size_t tmp=permutation[x1];
+        permutation[x1]=permutation[x2];
+        permutation[x2]=tmp;
+        return;
+    }
+    size_t *tmp=static_cast<size_t*>(realloc(M));
+    if(tmp==NULL)
+        throw(bad_alloc());
+    permutation=tmp;
+    // TODO: fill, or change operator[]
+    if (m<maximum) {
+        permutation[M]=m;
+    } else {
+        permutation[M]=permutation[m];
+    }
+    permutation[m]=M;
+    maximum=M;
 }
 void Permutation::swapExisting(const size_t x1, const size_t x2) noexcept
 {
-	if(x1>maximum||x2>maximum)
-		return;
-	size_t tmp=permutation[x1];
-	permutation[x1]=permutation[x2];
-	permutation[x2]=tmp;
+    if(x1>maximum||x2>maximum)
+        return;
+    size_t tmp=permutation[x1];
+    permutation[x1]=permutation[x2];
+    permutation[x2]=tmp;
 }             
 void Permutation::swap(Permutation &&other) noexcept
 {
-	size_t *tmpp=permutation;
-	size_t tmp=maximum;
-	permutation=other.permutation;
-	maximum=oter.maximum;
-	other.permutation=tmpp;
-	other.maximum=tmp;
+    size_t *tmpp=permutation;
+    size_t tmp=maximum;
+    permutation=other.permutation;
+    maximum=oter.maximum;
+    other.permutation=tmpp;
+    other.maximum=tmp;
 }
