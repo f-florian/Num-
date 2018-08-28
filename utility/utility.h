de8347747318b0d3cb083b353bd27bc192d05192
Francesco Florian <francesco.florian94@gmail.com>
2018-08-28 17:42:18 +0200

    fixes

--- utility/utility.h
+++ utility/utility.h
@@ -15,20 +15,32 @@
  * You should have received a copy of the GNU General Public License
  * along with Num++.  If not, see <http://www.gnu.org/licenses/>.
  ***************************************************************************/
+#ifndef UTILITY_H
+#define UTILITY_H
 
 #include <type_traits>
 
-namespace numpp {
+namespace Numpp {
+    /**
+     * Template functions for getting information about and manipulating enum classes
+     */
     namespace EnumUtils {
         /**
-         * Convert enum item to its numeric value
-         * @param enum item to convert
+         * Convert enum item to its numeric value.
+	 *
+         * @param item enum item to convert
          * @return converted value
          */
-        template <class T> constexpr std::enable_if<std::is_enum<T>::value, std::underlying_type<T>::type>::type enumToNumeric (T item) noexcept {
-            return static_cast<std::underlying_type<T>::type>(item);
+        template <class T> constexpr typename std::enable_if<std::is_enum<T>::value, typename std::underlying_type<T>::type>::type enumToNumeric (T item) noexcept {
+            return static_cast<typename std::underlying_type<T>::type>(item);
         }
-        template <class T> constexpr std::enable_if<std::is_enum<T>::value, size_t>::type size() noexcept {
+	/**
+	 * Get enum size.
+	 *
+	 * @return enum size (i.e. 1 + maximum value)
+	 * For this function to work the enum must have an item named `max` indicating maximum value
+	 */
+        template <class T> constexpr typename std::enable_if<std::is_enum<T>::value, size_t>::type size() noexcept {
             return static_cast<size_t>(T::max) + 1;
         }
     }
@@ -41,5 +53,7 @@ namespace numpp {
         dense = 0,
         sparse,
         max = sparse
-    }
+    };
 }
+
+#endif /* UTILITY_H */
