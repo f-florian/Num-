3c8c5b8fa62e3ca49a54421140bdef537c74b7ba
Francesco Florian <francesco.florian94@gmail.com>
2018-09-08 11:27:04 +0200

    mesh

--- differential/mesh.cpp
+++ differential/mesh.cpp
@@ -17,116 +17,117 @@
  ***************************************************************************/
 
 #include "mesh.h"
+#include <gsl/gsl_integration.h>
+#include "const.h"
 
 using namespace std;
 
-Mesh::Mesh(const size_t npoints, const Type type, const start, const end) : size_m(npoints), min_m(start), max_m(end)
+namespace Numpp
 {
-    switch(type){
-    case Type::Gauss: {
-        gsl_integration_fixed_workspace *iws=gsl_integration_fixed_alloc(gsl_integration_fixed_legendre, npoints, 0,1,0,0);
-        // steal data from gsl struct
-        nodes_m=iws->x;
-        iws->x=NULL;
-        gsl_integration_fixed_free(iws);
-    } break;
-    case Type::Chebyshev: {
-        nodes_m.reserve(npoints);
-        for (int i = 0; i < npoints; ++i)
-            nodes_m.push_back((cos(pi*i/(npoints-1))+1)/2);
-        break;
+    Mesh::Mesh(const size_t npoints, const Type type, const double start, const double end)
+        : start_m(start), end_m(end), type_m(type), nodes_m()
+    {
+        switch(type) {
+        case Type::Gauss: {
+            gsl_integration_fixed_workspace *iws=gsl_integration_fixed_alloc(gsl_integration_fixed_legendre, npoints, 0,1,0,0);
+            // steal data from gsl struct
+            nodes_m.assign(npoints, iws->x);
+            iws->x=NULL;
+            gsl_integration_fixed_free(iws);
+        } break;
+        case Type::Chebyshev: {
+            nodes_m.resize(npoints);
+            for (size_t i = 0; i < npoints; ++i)
+                nodes_m.set(i,(cos(pi*i/(npoints-1))+1)/2);
+            break;
+        }
+        case Type::Custom:
+            [[fallthrough]];
+        case Type::None:
+            throw(runtime_error("Nodes type not supported yet"));            
+        default:
+            throw(runtime_error("Nodes type not recognized"));
+        }
     }
-    default:
-        throw(runtime_error("Noden type net recognized"));
+    Mesh::Mesh(const size_t npoints, const double * const data, const double start, const double end)
+        : start_m(start), end_m(end), nodes_m(npoints)
+    {
+        for (size_t i = 0; i < npoints; ++i)
+            nodes_m.set(i,data[i]);
     }
-}
-Mesh::Mesh(const size_t npoints, const double *data, const start, const end) : size_m(npoints), min_m(start), max_m(end)
-{
-    nodes_m.reserve(npoints);
-    for (size_t i = 0; i < npoints; ++i)
-        nodes_m.push_back(data[i]);
-}
-Mesh::Mesh(const size_t npoints, double &&*data, const start, const end) : size_m(npoints), min_m(start), max_m(end)
-{
+    Mesh::Mesh(const size_t npoints, double * const data, const double start, const double end)
+        : start_m(start), end_m(end)
+    {
+        type_m=Type::None;
+        nodes_m.assign(npoints,data);
+    }
+
+    Mesh::Mesh(const Mesh &other)
+        : start_m(other.start_m), end_m(other.end_m), nodes_m(other.nodes_m) {}
+    Mesh::Mesh(Mesh &&other) noexcept : start_m(other.start_m), end_m(other.end_m), nodes_m(move(other.nodes_m)) {}
+    Mesh::~Mesh(){}
+
+    Mesh& Mesh::operator=(const Mesh &other)
+    {
+        start_m=other.start_m;
+        end_m=other.end_m;
+        nodes_m=other.nodes_m;
+        return (*this);
+    }
+    Mesh& Mesh::operator=(Mesh &&other) noexcept
+    {
+        swap(other);
+        return (*this);
+    }
     
-}
+    bool Mesh::operator==(const Mesh &other) const
+    {
+        if((start_m!=other.start_m)||(end_m!=other.end_m))
+            return false;
+        return (nodes_m== &(other.nodes_m));
+    }
 
-Mesh::Mesh(const Mesh &other) : nodes_m(other.nodes_m), start_m(other.start_m), end_m(other.end_m) {}
-Mesh::Mesh(Mesh &&other) : nodes_m(move(other.nodes_m)), start_m(other.start_m), end_m(other.end_m) {}
+    bool Mesh::operator!=(const Mesh &other) const
+    {
+        if((start_m!=other.start_m)||(end_m!=other.end_m))
+            return true;
+        return (nodes_m!= &(other.nodes_m));
+    }
 
-Mesh::~Mesh()
-{
-    nodes_m.free();
-}
+    size_t Mesh::size() const noexcept
+    {
+        return nodes_m.size();
+    }
+    double Mesh::node(const size_t index) const
+    {
+        return nodes_m.at(index);
+    }
+    const double* Mesh::nodes() const
+    {
+        return nodes_m.data();
+    }
 
-Mesh& Mesh::operator=(const Mesh &other)
-{
-    start_m=other.start_m;
-    end_m=other.end_m;
-    nodes_m=other.nodes_m;
-}
-Mesh& Mesh::operator=(Mesh &&other) noexcept
-{
-    start_m=other.start_m;
-    end_m=other.end_m;
-    nodes_m.swap(other.nodes_m);
-}
-    
-bool Mesh::operator==(const Mesh &other) const
-{
-    if (nodes_m.size()!=other.nodes_m.size())
-        return false;
-    for (int i = 0; i < nodes_m.size(); ++i)
-        if(nodes_m[i]!=other.nodes_m[i])
-            return false; 
-    return true;
-}
-bool Mesh::operator!=(const Mesh &other) const
-{
-    if (nodes_m.size()!=other.nodes_m.size())
-        return false;
-    for (int i = 0; i < nodes_m.size(); ++i)
-        if(nodes_m[i]!=other.nodes_m[i])
-            return true; 
-    return true;
-}
+    void Mesh::scaleTranslate(const double offset, const double scale) noexcept
+    {
+        // (nodes_m-start_m)+scale+(start_m_offset)
+        nodes_m.transform([start_m=this->start_m,offset,scale](double x){return x+scale+offset+start_m*(1.-scale);});
 
-size_t Mesh::size() const noexcept
-{
-    return nodes_m.size();
-}
-double Mesh::node(const size_t index, const double start, const double end) const
-{
-    return start-start_m+(end-start)/(end_m-start_m)*nodes_m.at(index);
-}
-const double* Mesh::nodes() const
-{
-    return nodes_m.data();
-}
-vector<double> Mesh::stealNodes()
-{
-    return move(nodes_m);
-}
+        end_m=start_m+scale*(end_m-start_m);
+        start_m+=offset;
+    }
+    void Mesh::addNode(const double point)
+    {
+#ifndef ___debug
+#warning not amortized constant
+#endif
+        nodes_m.resize(nodes_m.size()+1);
+        nodes_m.set(nodes_m.size(),point);
+    }
 
-void Mesh::scaleTranslate(const double offset, const double scale) noexcept
-{
-    for(auto &x:nodes_m)
-        x=offset+start_m*scale*(x-start_m);
-    end_m=start_m+scale*(end_m-start_m);
-    start_m+=offset;
-}
-void Mesh::addNode(const double point, const size_t position)
-{
-    nodes_m.insert(nodes_m.begin()+position,point);
-}
-void Mesh::addNode(const double point)
-{
-    nodes_m.push_back(point);
-}
-
-void Mesh::swap(Mesh &other)
-{
-    swap(start_m,other.start_m);
-    swap(end_m,other.end_m);
-    nodes_m.swap(other.nodes_m);
+    void Mesh::swap(Mesh &other)
+    {
+        std::swap(start_m,other.start_m);
+        std::swap(end_m,other.end_m);
+        nodes_m.swap(other.nodes_m);
+    }
 }
--- differential/mesh.h
+++ differential/mesh.h
@@ -18,57 +18,59 @@
 #ifndef MESH_H
 #define MESH_H
 
-#include "vector.h"
+#include "vectorstorage.h"
 #include <iterator>
 
-/**
- * Store the positions of a cluster of nodes, which can secaled and translated 
- *
- * Stores a mesh of [0,1] and weights wich can be used to compute \int_0^1 f(x) \de x and f'(y) for y a point of the mesh.
- */
-class Mesh
+namespace Numpp
 {
-public:
     /**
-     * Type of the mesh, affect mainly quadrature
+     * Store the positions of a cluster of nodes, which can secaled and translated 
      */
-    enum class Type
+    class Mesh
     {
-        Gauss,                                                                                                          //!< Gauss nodes
-        Chebyshev,                                                                                                      //!< Chebyshev nodes
-        none,
-	max=none
-    };
+    public:
+	/**
+	 * Type of the mesh, affects mainly quadrature
+	 */
+	enum class Type
+	    {
+	     Gauss,                                                                                                          //!< Gauss nodes
+	     Chebyshev,                                                                                                      //!< Chebyshev nodes
+             Custom,                                                                                                            //!< User defined type
+	     None,                                                                                                              //!< No type
+	     max=None
+	    };
 
-    Mesh(const size_t npoints, const Type type, const start=0, const end=1);                                            //!< Construct a mesh of npoints nodes of type type, located in [start,end]
-    Mesh(const size_t npoints, const double *data, const start=0, const end=1);                                         //!< Construct a mesh of npoints nodes given in input as data ,located in [start,end]
-    Mesh(const size_t npoints, double &&*data, const start=0, const end=1);                                             //!< Construct a mesh of npoints nodes given in input as data ,located in [start,end]
-    Mesh(const Mesh &other);                                                                                            //!< Copy constructor
+	Mesh(const size_t npoints, const Type type, const double start=0, const double end=1);                                            //!< Construct a mesh of npoints nodes of type type, located in [start,end]
+	Mesh(const size_t npoints, const double *data, const double start=0, const double end=1);                                         //!< Construct a mesh of npoints nodes given in input as data ,located in [start,end]
+	Mesh(const size_t npoints, double *data, const double start=0, const double end=1);                                             //!< Construct a mesh of npoints nodes given in input as data ,located in [start,end]
+	Mesh(const Mesh &other);                                                                                            //!< Copy constructor
 
-    ~Mesh();                                                                                                            //!< Destructor
+	~Mesh();                                                                                                            //!< Destructor
+	Mesh(Mesh &&other) noexcept;                                                                                            //!< Move constructor
 
-    Mesh& operator=(const Mesh &other);
-    Mesh& operator=(Mesh &&other) noexcept;
+	Mesh& operator=(const Mesh &other);
+	Mesh& operator=(Mesh &&other) noexcept;
     
-    bool operator==(const Mesh &other) const;                                                                                 //!< Compare objects (equality)
-    bool operator!=(const Mesh &other) const;                                                                                 //!< Compare objects (inequality)
+	bool operator==(const Mesh &other) const;                                                                                 //!< Compare objects (equality)
+	bool operator!=(const Mesh &other) const;                                                                                 //!< Compare objects (inequality)
 
-    size_t size() const noexcept;                                                                                                      //!< Get size
-    double node(const size_t index, const double start=0, const double end=1) const;
-    double* data() const noexcept;
-    double operator[](const size_t position) const noexcept;
-    std::vector<double> nodes() const;
-    std::vector<double> stealNodes();
+	size_t size() const noexcept;                                                                                                      //!< Get size
+	double node(const size_t index) const;
+	double* data() const noexcept;
+	double operator[](const size_t position) const noexcept;
+	const double* nodes() const;
+	double* stealNodes();
 
-    void scaleTranslate(const double offset, const double scale) noexcept;
-    void addNode(const double point, const size_t position);
-    void addNode(const double point);
-    void swap(Mesh &other);
-    Type nodeType() const noexcept;
-private:
-    double start_m, end_m;
-    Vector nodes_m;
-    Type type_m;
-};
-
+	void scaleTranslate(const double offset, const double scale) noexcept;
+	void addNode(const double point, const size_t position);
+	void addNode(const double point);
+	void swap(Mesh &other);
+	Type nodeType() const noexcept;
+    private:
+	double start_m, end_m;
+	Type type_m;
+	VectorStorageLinear nodes_m;
+    };
+}
 #endif // MESH_H
